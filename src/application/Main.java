a884e7b418c2e8b3456be7697039357018c32fc1
Balazs Bekesi <Balazs_Bekesi@epam.com>
2017-08-31 13:26:57 +0200

    ui improvements

--- src/application/Main.java
+++ src/application/Main.java
@@ -2,19 +2,19 @@ package application;
 
 import static application.shared.Constants.MAP_HEIGHT;
 import static application.shared.Constants.MAP_WIDTH;
+import static application.shared.Constants.SETTINGS_LOAD_PRETRAINED_NETWORK;
 
 import application.map.Map;
-import application.shared.Constants;
 import application.shared.PropertiesForBinding;
 import application.view.ViewManager;
 import javafx.application.Application;
+import javafx.beans.binding.Bindings;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.effect.BlendMode;
-import javafx.scene.input.KeyCode;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.CycleMethod;
@@ -41,50 +41,7 @@ public class Main extends Application {
 			Group mapGroup = map.getMapGroup();
 			view = new ViewManager(map);
 
-			Group buttonGroup = new Group();
-			buttonGroup.setTranslateX(510);
-			buttonGroup.setTranslateY(230);
-
-			Button startButton = new Button("start");
-			buttonGroup.getChildren().add(startButton);
-
-			startButton.setOnAction(new EventHandler<ActionEvent>() {
-				@Override
-				public void handle(ActionEvent e) {
-					Constants.SETTINGS_LOAD_PRETRAINED_NETWORK = false;
-					view.initTimeline();
-					view.run();
-				}
-			});
-
-			Button stopButton = new Button("stop");
-			buttonGroup.getChildren().add(stopButton);
-			stopButton.setTranslateX(130);
-
-			stopButton.setOnAction(new EventHandler<ActionEvent>() {
-				@Override
-				public void handle(ActionEvent e) {
-					view.stop();
-					PropertiesForBinding.populationProperty.set(0);
-				}
-			});
-
-			Button startPretrained = new Button("continue");
-			buttonGroup.getChildren().add(startPretrained);
-			startPretrained.setTranslateY(60);
-			startPretrained.setTranslateX(40);
-
-			startPretrained.setOnAction(new EventHandler<ActionEvent>() {
-				@Override
-				public void handle(ActionEvent e) {
-					Constants.SETTINGS_LOAD_PRETRAINED_NETWORK = true;
-					// view.restart();
-					view.initTimeline();
-					view.run();
-				}
-			});
-
-			addControlOptions(scene, map);
+			Group buttonGroup = createButtons();
 
 			Rectangle colors = new Rectangle(MAP_WIDTH, MAP_HEIGHT,
 					new LinearGradient(0f, 1f, 1f, 0f, true, CycleMethod.NO_CYCLE,
@@ -95,25 +52,16 @@ public class Main extends Application {
 
 			colors.setBlendMode(BlendMode.OVERLAY);
 
-			Text text = new Text();
-			text.setTranslateX(310);
-			text.setTranslateY(300);
-			text.setFill(Color.WHITE);
-			text.setId("text");
-			text.setOpacity(0.4);
-			text.textProperty().bind(PropertiesForBinding.populationProperty.add(1).asString().concat(". generation "));
+			Text text = createText(310, 260);
+			text.textProperty()
+					.bind(Bindings.format("generation : %s#", PropertiesForBinding.populationProperty.add(1)));
 
-			Text text2 = new Text();
-			text2.setTranslateX(310);
-			text2.setTranslateY(330);
-			text2.setFill(Color.WHITE);
-			text2.setId("text");
-			text2.setOpacity(0.4);
-			text2.textProperty()
-					.bind(PropertiesForBinding.topFitnessProperty.asString().concat("  fitness "));
+			Text text2 = createText(310, 290);
+			text2.textProperty().bind(Bindings.format("   fitness : %s", PropertiesForBinding.topFitnessProperty));
 
-			Group effectedGroup = new Group(new Rectangle(MAP_WIDTH, MAP_HEIGHT, Color.BLACK), new Group(mapGroup),
-					text2, text, colors);
+			Group effectedGroup = new Group(new Rectangle(MAP_WIDTH, MAP_HEIGHT, Color.rgb(10, 10, 10)),
+					new Group(mapGroup), text2, text, colors);
+
 			root.getChildren().add(effectedGroup);
 			root.getChildren().add(buttonGroup);
 
@@ -124,30 +72,63 @@ public class Main extends Application {
 		}
 	}
 
-	/*
-	 * For test phase only
-	 */
-	private void addControlOptions(Scene scene, Map map) {
-		scene.setOnKeyPressed(e -> {
-			// if (e.getCode() == KeyCode.LEFT) {
-			// map.leftPressed.set(true);
-			// }
-			// if (e.getCode() == KeyCode.RIGHT) {
-			// map.rightPressed.set(true);
-			// }
-		});
+	private Group createButtons() {
+		Group buttonGroup = new Group();
+		buttonGroup.setTranslateX(510);
+		buttonGroup.setTranslateY(230);
 
-		scene.setOnKeyReleased(e -> {
-			// if (e.getCode() == KeyCode.LEFT) {
-			// map.leftPressed.set(false);
-			// }
-			if (e.getCode() == KeyCode.SPACE) {
+		Button startButton = new Button("  start  ");
+		Button stopButton = new Button("stop");
+		Button startPretrained = new Button(" continue");
+		Button triggerNextMap = new Button("next");
+
+		buttonGroup.getChildren().add(startButton);
+		buttonGroup.getChildren().add(stopButton);
+		buttonGroup.getChildren().add(startPretrained);
+		buttonGroup.getChildren().add(triggerNextMap);
+
+		stopButton.setTranslateX(150);
+		startPretrained.setTranslateY(40);
+		triggerNextMap.setTranslateX(150);
+		triggerNextMap.setTranslateY(40);
+
+		startButton.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent e) {
+				SETTINGS_LOAD_PRETRAINED_NETWORK = false;
+				view.initTimeline();
 				view.run();
 			}
-			// if (e.getCode() == KeyCode.RIGHT) {
-			// map.rightPressed.set(false);
-			// }
 		});
+
+		stopButton.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent e) {
+				view.stop();
+				PropertiesForBinding.populationProperty.set(0);
+			}
+		});
+
+		startPretrained.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent e) {
+				SETTINGS_LOAD_PRETRAINED_NETWORK = true;
+				view.initTimeline();
+				view.run();
+			}
+		});
+
+		return buttonGroup;
+	}
+
+	private Text createText(int x, int y) {
+		Text text = new Text();
+		text.setTranslateX(x);
+		text.setTranslateY(y);
+		text.setFill(Color.WHITE);
+		text.setId("text");
+		text.setOpacity(0.4);
+		return text;
 	}
 
 	public static void main(String[] args) {
